4447c71b0deddfefd1f569bd8f26720eb31076c7
dausuke <ksye616@gmail.com>
2021-08-04 00:07:35 +0900

    Extract Components

---
+++ src/components/CircleButton.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { string } from 'prop-types';
+
+const CircleButton = (props) => {
+  const { children } = props;
+  return (
+    <View style={styles.circleBtton}>
+      <Text style={styles.circleBttonLabel}>{children}</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  circleBtton: {
+    backgroundColor: '#467FD3',
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    elevation: 8,
+  },
+  circleBttonLabel: {
+    color: '#ffffff',
+    fontSize: 40,
+    lineHeight: 40,
+  },
+});
+CircleButton.propTypes = {
+  children: string.isRequired,
+};
+export default CircleButton;
