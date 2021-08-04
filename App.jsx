23e21b34c53bd7a6b9f4954f2d7b84361780e87b
dausuke <ksye616@gmail.com>
2021-08-04 15:51:04 +0900

    Creat LogInScreen&SignUpScreen

--- App.jsx
+++ App.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 // import MemoListScreen from './src/screens/MemoListScreens';
 // import MemoDitailScreen from './src/screens/MemoDitailScreens';
-import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
 // import MemoCreatScreen from './src/screens/MemoCreatScreen';
+// import LogInScreen from './src/screens/LogInScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
 
 export default function App() {
-  return <MemoEditScreen />;
+  return <SignUpScreen />;
 }
---
+++ src/screens/SignUpScreen.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+
+import AppBar from '../components/AppBar';
+import Button from '../components/Button';
+
+const SignUpScreen = () => {
+  return (
+    <View style={styles.container}>
+      <AppBar />
+      <View style={styles.inner}>
+        <Text style={styles.title}>Sign Up</Text>
+        <TextInput value="email" style={styles.input} />
+        <TextInput value="password" style={styles.input} />
+        <Button label="Submit" />
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Already registered?</Text>
+          <Text style={styles.footerLink}>Log In</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F0F4F8',
+  },
+  inner: {
+    paddingVertical: 24,
+    paddingHorizontal: 27,
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    marginBottom: 24,
+  },
+  input: {
+    fontSize: 16,
+    height: 48,
+    borderColor: '#DDDDDD',
+    borderWidth: 1,
+    backgroundColor: '#ffffff',
+    paddingHorizontal: 8,
+    marginBottom: 16,
+  },
+  footer: {
+    flexDirection: 'row',
+  },
+  footerText: {
+    fontSize: 14,
+    lineHeight: 24,
+    marginRight: 8,
+  },
+  footerLink: {
+    fontSize: 14,
+    lineHeight: 24,
+    color: '#467FE3',
+  },
+});
+export default SignUpScreen;
