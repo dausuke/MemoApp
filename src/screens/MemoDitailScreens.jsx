5c610b5cd1e3ccda4e5ca9f28eab22b8547ab22f
dausuke <ksye616@gmail.com>
2021-08-07 23:50:49 +0900

    Update memo data on MemoEditScreen

--- src/screens/MemoDitailScreens.jsx
+++ src/screens/MemoDitailScreens.jsx
@@ -46,7 +46,7 @@ const MemoDitailScreen = (props) => {
         style={{ top: 60, bottom: 'auto' }}
         name="edit-2"
         onPress={() => {
-          navigation.navigate('MemoEdit');
+          navigation.navigate('MemoEdit', { id: memo.id, bodyText: memo.bodyText });
         }}
       />
     </View>
